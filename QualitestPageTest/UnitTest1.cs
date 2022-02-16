5425a497a2f21e56ae932d8c775b7cf57c61c835
Tamer <Masarwe.tamer@gmail.com>
2022-02-16 02:07:59 +0200

    Additional Test Method

--- QualitestPageTest/UnitTest1.cs
+++ QualitestPageTest/UnitTest1.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Text;
 using QualitestPage;
 using System.Collections.Generic;
+using System.Web;
 
 [assembly: Parallelizable(ParallelScope.All)]
 
@@ -18,15 +19,16 @@ namespace QualitestPageTest
     public class QualitestPageTest
     {
         List<User> usersList;
+        string NavigationPage = "https://qualitestgroup.com/contact-us/";
 
         [OneTimeSetUp]
         public void Initialize() 
         {
-            usersList = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("DataFile.json"));// I'm trying to read the Json file, then to start testing   
+            usersList = JsonConvert.DeserializeObject<List<User>>(File.ReadAllText("DataFile.json"));  
         }
 
-        [Test, Category("Empty Fields")]  
-        public void SendRequestWithEmptyField_SentFaild_ReturnsTrue()//UI test that validates if there's a field tha's empty
+        [Test, Category("Negative Test - Empty Fields")]  
+        public void SendRequestWithEmptyField_SentFaild_ReturnsTrue()
         {
             IWebDriver driver = new ChromeDriver();
             SelectElement selectElement;
@@ -50,21 +52,23 @@ namespace QualitestPageTest
                 driver.FindElement(By.Id("phone-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(a.PhoneNumber);
 
                 selectElement = new SelectElement(driver.FindElement(By.Id("location-34dd68e0-b077-4e95-9243-b861f3f2fd7d")));          
-                if(!(a.Location.Length == 0))selectElement.SelectByText(a.Location);
-                
+                if(!(a.Location.Length == 0))selectElement.SelectByText(a.Location);              
 
                 driver.FindElement(By.Id("email-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
                 driver.FindElement(By.Id("email-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(a.Email);
                 driver.FindElement(By.CssSelector("#hsForm_34dd68e0-b077-4e95-9243-b861f3f2fd7d > div > div.actions > input")).Click();
 
                 if (!(driver.FindElement(By.CssSelector("#hsForm_34dd68e0-b077-4e95-9243-b861f3f2fd7d > div > div.actions > input")).Displayed))
-                    Assert.IsTrue(false);
+                {
+                    CleanUp(driver);
+                    Assert.IsTrue(false); 
+                }
             }
             Assert.IsTrue(true);
             CleanUp(driver);
         }
 
-        [Test, Category("Invalid Inputs")]
+        [Test, Category("Negative Test - Invalid Inputs")]
         public void InValidPhoneNumber_SentFail_ReturnsTrue()
         {
             IWebDriver driver = new ChromeDriver();
@@ -73,25 +77,19 @@ namespace QualitestPageTest
 
             NavigateToContactUsPage(driver);
 
-            driver.FindElement(By.Id("firstname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
             driver.FindElement(By.Id("firstname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.FirstName);
 
-            driver.FindElement(By.Id("lastname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
             driver.FindElement(By.Id("lastname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.LastName);
 
-            driver.FindElement(By.Id("company-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
             driver.FindElement(By.Id("company-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.CompanyName);
 
             driver.FindElement(By.Id("what_would_you_like_to_talk_about_0-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Click();
 
-            driver.FindElement(By.Id("phone-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
             driver.FindElement(By.Id("phone-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.PhoneNumber);
 
             selectElement = new SelectElement(driver.FindElement(By.Id("location-34dd68e0-b077-4e95-9243-b861f3f2fd7d")));
             selectElement.SelectByText(user.Location);
 
-
-            driver.FindElement(By.Id("email-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Clear();
             driver.FindElement(By.Id("email-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.Email);
             driver.FindElement(By.CssSelector("#hsForm_34dd68e0-b077-4e95-9243-b861f3f2fd7d > div > div.actions > input")).Click();
 
@@ -103,14 +101,49 @@ namespace QualitestPageTest
             Assert.IsTrue(true);
             CleanUp(driver);
         }
+        
+        [Test, Category("Positive Test")]
+        public void ValidUser_SentFail_ReturnsTrue()
+        {
+            IWebDriver driver = new ChromeDriver();
+            SelectElement selectElement;
+            User user = new User { FirstName = "Tamer", LastName = "Masarwe", Email = "A.B@microsoft.com", CompanyName = "Qualitest", Location = "Middle East", PhoneNumber = "050-3172231" };
+
+            NavigateToContactUsPage(driver);
+       
+            driver.FindElement(By.Id("firstname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.FirstName);
+            
+            driver.FindElement(By.Id("lastname-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.LastName);
+
+            driver.FindElement(By.Id("company-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.CompanyName);
+
+            driver.FindElement(By.Id("what_would_you_like_to_talk_about_0-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).Click();
+
+            driver.FindElement(By.Id("phone-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.PhoneNumber);
+
+            selectElement = new SelectElement(driver.FindElement(By.Id("location-34dd68e0-b077-4e95-9243-b861f3f2fd7d")));
+            selectElement.SelectByText(user.Location);
+
+            driver.FindElement(By.Id("email-34dd68e0-b077-4e95-9243-b861f3f2fd7d")).SendKeys(user.Email);
+            driver.FindElement(By.CssSelector("#hsForm_34dd68e0-b077-4e95-9243-b861f3f2fd7d > div > div.actions > input")).Click();
+
+            if (HttpUtility.ParseQueryString(new Uri(driver.Url).Query).Equals(NavigationPage))
+            {
+                Assert.IsTrue(false);
+                CleanUp(driver);
+            }
+
+            Assert.IsTrue(true);
+            CleanUp(driver);
+        }
+
 
        public void NavigateToContactUsPage(IWebDriver driver) 
         {
-            driver.Navigate().GoToUrl("https://qualitestgroup.com/contact-us/");
+            driver.Navigate().GoToUrl(NavigationPage);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
 
-        
         public void CleanUp(IWebDriver driver)
         {
             driver.Quit();
